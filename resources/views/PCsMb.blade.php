247f12cae5a09a2d015c980ed6c7d489f1717f38
Tu Angga <anggawidhyaputu@gmail.com>
2025-07-22 10:39:37 +0700

    PCs Motherboard

--- resources/views/PCsMb.blade.php
+++ resources/views/PCsMb.blade.php
@@ -8,15 +8,19 @@
 @endpush
 
 @section('content')
-<main> 
-    {{-- Hero Section --}}
+<main>
     <section class="hero">
-        <div class="slider-wrapper">
-            <div class="slider">
-                <img src="{{ asset('image/slider/x870.jpg') }}" alt="Asus X870 Motherboard">
-                <img src="{{ asset('image/slider/x870e.jpg') }}" alt="Asus X870E Motherboard">                
-                <img src="{{ asset('image/slider/z890.jpg') }}" alt="Asus Z890 Motherboard">
+        <div class="relative mx-0" >
+            <div class="flex">
+                <img id="slide 1" src="{{ asset('image/slider/x870.jpg') }}" alt="Asus X870 Motherboard">
+                <img id="slide 2" src="{{ asset('image/slider/x870e.jpg') }}" alt="Asus X870E Motherboard">
+                <img id="slide 3" src="{{ asset('image/slider/z890.jpg') }}" alt="Asus Z890 Motherboard">
             </div>
+                <div class="slider-hero">
+                    <a href="#slide 1"></a>
+                    <a href="#slide 2"></a>
+                    <a href="#slide 3"></a>
+                </div>
         </div>
     </section>
-</main>
+</main> 
